7200b035da0972edffb3d22b01bfda673b99ea0d
Mohammed armaan <mohammedarmaan0712@gmail.com>
2025-02-15 23:30:10 -0500

    Add my project code

---
+++ test.py
@@ -0,0 +1,52 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """"
+Answer the question below:
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
+model = OllamaLLM(model = "llama3")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def handle_conversation ():
+    context = ""
+    print ("Whelcome to CheatGPT? Type exit to quit.")
+    while True:
+        user_input = input ("You: ")
+        if user_input == "exit":
+            break
+        answer = chain.invoke({"context":context, "question":user_input})
+        print ("Bot:", answer)
+
+        context += f"User: {user_input} \nBot: {answer}"
+
+if __name__ == "__main__":
+    handle_conversation()
+
+
+
+
+
+
+
+
+
+
+'''
+Tasks to do (Pseudocode):
+
+Accept a pdf file
+
+Convert the pdf file to a format that can be used to train LLama
+
+Using the merged model, answer questions specific to the data from the pdf.
+
+Other use cases:
+--> Use the model to predict the type of questions one will be tested on 
+'''

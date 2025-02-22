e081c0e71c5fc453d56e768f670c5e31d53e9ef2
Mohammed armaan <mohammedarmaan0712@gmail.com>
2025-02-22 14:01:12 -0500

    Updated project with helper functions

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+import PyPDF2
+import os
 
 template = """"
 Answer the question below:
@@ -26,10 +28,53 @@ def handle_conversation ():
 
         context += f"User: {user_input} \nBot: {answer}"
 
+
+
+def parce_pdf_to_text(pdf_name):
+    # Check if pdf_name.txt exists:
+   ''' if not (os.path.exists(pdf_name+".pdf")):
+        raise ValueError("PDF name should refer to a file in the project directory.")  # Custom error message
+
+    filename = pdf_name+".txt"
+
+    if os.path.exists(filename):
+        with open(filename, "a", encoding="utf-8") as file:
+            #Locate PDF
+            with open(pdf_name+".pdf", "rb") as pdf_file:
+                reader = PyPDF2.PdfReader(pdf_file)
+                for page_num in range(len(reader.pages)):
+                    page = reader.pages[page_num]
+                    file.write(page.extract_text())
+    else:
+        with open(filename, "w", encoding="utf-8") as file:
+            #Locate PDF
+            with open(pdf_name+".pdf", "rb") as pdf_file:
+                reader = PyPDF2.PdfReader(pdf_file)
+                for page_num in range(len(reader.pages)):
+                    page = reader.pages[page_num]
+                    file.write(page.extract_text())'''
+   filename = pdf_name+".txt"
+   try:
+       with open(pdf_name+".pdf", "rb") as pdf_file:
+            with open(filename, return_open_mode_for_txt_files(filename), encoding="utf-8") as file:
+                 reader = PyPDF2.PdfReader(pdf_file)
+                 for page_num in range(len(reader.pages)):
+                     page = reader.pages[page_num]
+                     file.write(page.extract_text())
+   except FileNotFoundError:
+       print(f"Error: The file '{pdf_name}' was not found.")
+       return
+
+def return_open_mode_for_txt_files(filename):
+    if os.path.exists(filename):
+        return 'a'
+    else:
+        return 'w'
+
+    
+
 if __name__ == "__main__":
-    handle_conversation()
-
-
+    parce_pdf_to_text("test")
 
 
 
